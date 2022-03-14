98d9fa8d264f7ff7d8c791d6e2a004da6d6a2edd
Liam Watts <elnewatts@gmail.com>
2022-03-14 16:49:34 +0000

    Desc now under title

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,7 +16,9 @@
         <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
             <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">CR Cache</h1>
             <div class="h-1 w-20 bg-indigo-500 rounded"></div>
-
+            <div class="mb-1">
+                <p class="lg:w-2/3 text-xl leading-relaxed">Express js with Redis</p>
+            </div>
             <div class="mt-4">
                 <h2 class="title-font text-xl">Your Location:</h2>
                 {#if photos && photos.geo}
@@ -34,12 +36,10 @@
                 {/if}
             </div>
 
-            <div class="flex flex-col w-full mt-5 mb-1">
-                <p class="lg:w-2/3 leading-relaxed text-base">Express js with Redis</p>
-            </div>
+           
         </div>
         <div class="lg:w-2/3 w-full overflow-auto">
-            <table class="table-auto w-full text-left whitespace-no-wrap">
+            <table class="table- w-full text-left whitespace-no-wrap">
                 <thead>
                 <tr>
                     <th class="px-4 py-3 title-font tracking-wider font-medium text-white text-sm bg-gray-800 rounded-tl rounded-bl">Title</th>
