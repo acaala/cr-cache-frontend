c78c7b8d7aa1a0c4764f736be9da84e8e9fa3baf
Liam Watts <elnewatts@gmail.com>
2022-03-14 11:23:23 +0000

    first commit

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,6 @@
+<script>
+	import '../app.css';
+</script>
+<div class="h-screen bg-gray-900">
+	<slot />
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import Table from '$lib/Table.svelte';
+    import { onMount } from 'svelte';
+    let photos: any;
+    const getPhotos = async () => {
+        photos = await fetch('http://localhost:3030/cachePhotos').then(r => r.json());
+    }
+    const clearPhotosCache = async () => {
+        photos = await fetch('http://localhost:3030/clearCachePhotos').then(r => r.json());
+    }
+    onMount(getPhotos)
+</script>
+<div class="text-gray-400 body-font">
+    <div class="container px-5 py-24 mx-auto">
+        <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">CR Cache</h1>
+            <div class="h-1 w-20 bg-indigo-500 rounded"></div>
+            <div class="flex flex-col w-full mt-5 mb-1">
+                <p class="lg:w-2/3 leading-relaxed text-base">Express js with Redis</p>
+            </div>
+        </div>
+            {#if photos}
+                <Table on:refetch={getPhotos} on:clearCache={clearPhotosCache} currentTime={photos.time} uncachedTime={photos.uncached}/>
+            {/if}    
+    </div>
+</div>
+
+
