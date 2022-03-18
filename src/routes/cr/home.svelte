1c55649916fced8cf10b6a69e4b44cbb1a234134
Liam Watts <elnewatts@gmail.com>
2022-03-18 10:33:06 +0000

    added JSscripts in

--- src/routes/cr/home.svelte
+++ src/routes/cr/home.svelte
@@ -3,20 +3,45 @@
     import { onMount } from "svelte";
     import { useApi } from '../../hooks/api';
 
-    let html: any
+
+    let html: any;
 
     onMount(async () => {
         html = await useApi('cr-home');
-        console.log(html)
+        console.log(html);
+        loadScript();
     })
+    const loadScript = () => {
+      const script = document.createElement('script')
+      script.src = 'https://development.coinrivet.com/wp-content/themes/coinrivet/assets/scripts/main.js?v=1.0.79';
+      const secondScript = document.createElement('script');
+      script.src ='https://development.coinrivet.com/wp-content/themes/coinrivet/assets/scripts/landing.js?v=1.0.79';
+      document.head.appendChild(script)
+      document.head.appendChild(secondScript)
+    }
+
+    
+
+
+
 </script>
 
 <div>
- 
     {#if html != undefined}
-        <Header time={html.time} uncachedTime={html.uncachedTime} on:refetch={async () => {html = await useApi('cr-home-clear')}} on:clearCache={async () => {html = await useApi('cr-home')}} />
+        <Header time={html.time} uncachedTime={html.uncachedTime} on:refetch={async () => {html = await useApi('cr-home-clear'); console.log(html)}} on:clearCache={async () => {html = await useApi('cr-home')}} />
         {@html html.response}
+
     {:else}
         <h2 class="text-gray-600 text-4xl mt-10">Loading</h2>
     {/if}
+
 </div>
+
+<style>
+    .cr-landing__hero__img__primary__video-container {
+        clip-path: none;
+        -webkit-clip-path: none;
+        position: relative;
+    }
+
+</style>
