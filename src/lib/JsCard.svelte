7107fbb7b0913215a5dee2f8d61ff16e9a6563db
Liam Watts <elnewatts@gmail.com>
2022-03-21 13:03:42 +0000

    added jquery  and DANGER ZOOONE

---
+++ src/lib/JsCard.svelte
@@ -0,0 +1,56 @@
+<script>
+	import { useApi, loadScript } from '../hooks/api';
+	import { onMount } from 'svelte';
+
+	export let title;
+	export let label;
+	export let tag;
+	let size;
+
+	onMount(async () => {
+		size = await useApi(label);
+	});
+	const clearCache = async () => {
+		await useApi(`${tag}-clear`);
+		size = '-';
+	};
+
+	const refetch = async () => {
+		loadScript(tag);
+		size = await useApi(label);
+	};
+</script>
+
+<div class="xl:w-1/3 md:w-1/2 p-4">
+	<div class="border border-gray-200 p-6 rounded-lg">
+		<div
+			class="w-10 h-10 inline-flex items-center justify-center rounded-full bg-indigo-100 text-indigo-500 mb-4"
+		>
+			<svg
+				fill="none"
+				stroke="currentColor"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="2"
+				class="w-6 h-6"
+				viewBox="0 0 24 24"
+			>
+				<path d="M22 12h-4l-3 9L9 3l-3 9H2" />
+			</svg>
+		</div>
+		<h2 class="text-lg text-gray-900 font-medium title-font mb-2">{title}</h2>
+		<div>
+			<p class="text-xs title-font font-medium text-gray-400 mb-1">
+				<span class="tracking-widest">Size: </span>{size} Bytes
+			</p>
+			<button
+				class="flex mt-4 text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg"
+				on:click={clearCache}>Clear Cache</button
+			>
+			<button
+				class="flex mt-4 text-white bg-gray-500 border-0 py-2 px-8 focus:outline-none hover:bg-gray-600 rounded text-lg"
+				on:click={refetch}>Refetch</button
+			>
+		</div>
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
+	import JsCard from '$lib/JsCard.svelte';
 	import PageCard from '$lib/PageCard.svelte';
+
+	import { useApi } from '../hooks/api';
+
+	let flushed: string;
+	let clearCacheFor: string;
+	let cacheCleared: boolean = false;
 </script>
 
 <section class="text-gray-600 body-font">
@@ -14,6 +21,73 @@
 			<PageCard title={'News'} link={'/cr/news'} label={'cr-news'} />
 			<PageCard title={'NFT'} link={'/cr/nft'} label={'cr-nft'} />
 			<PageCard title={'Support'} link={'/cr/support'} label={'cr-support'} />
+		</div>
+	</div>
+</section>
+
+<section class="text-gray-600 body-font">
+	<div class="container px-5 py-24 mx-auto">
+		<div class="flex flex-wrap w-full mb-10 flex-col items-center text-center">
+			<h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Javascripts</h1>
+		</div>
+		<div class="flex flex-wrap -m-4">
+			<JsCard title={'Main.js'} label={'js-main-info'} tag={'js-main'} />
+			<JsCard title={'Landing.js'} label={'js-landing-info'} tag={'js-landing'} />
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container mx-auto mb-4">
+		<div class="flex flex-wrap w-full mb-10 flex-col items-center text-center ">
+			<h1 class="sm:text-3xl text-2xl font-medium title-font text-gray-900">Danger Zone</h1>
+		</div>
+		<div class="border border-red-600 px-4 shadow rounded-lg lg:w-1/2 lg:mx-auto mx-4">
+			<div class="flex items-center border-b border-gray-200 pb-2 my-2 justify-between">
+				<div>
+					<h3 class="text-lg title-font font-medium text-gray-500 tracking-wider">Clear Cache</h3>
+					<p class="text-gray-500">
+						Clear For: <span
+							><select bind:value={clearCacheFor} name="cache" id="cache">
+								<optgroup label="Pages">
+									<option value="cr-home">Home</option>
+									<option value="cr-prices">Prices</option>
+									<option value="cr-news">News</option>
+									<option value="cr-nft">Nft</option>
+									<option value="cr-supprt">Support</option>
+								</optgroup>
+								<optgroup label="Javascripts">
+									<option value="js-main">Main.js</option>
+									<option value="js-landing">Landing.js</option>
+								</optgroup>
+							</select></span
+						>
+					</p>
+				</div>
+				<div>
+					<p class="text-red-500">{cacheCleared ? 'Cache Cleared' : ''}</p>
+				</div>
+				<button
+					class="flex text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg"
+					on:click={async () => {
+						await useApi(`${clearCacheFor}-clear`);
+						cacheCleared = true;
+					}}>Clear Cache</button
+				>
+			</div>
+			<div class="flex items-center border-b border-gray-200 py-2 my-2 justify-between">
+				<div>
+					<h3 class="text-lg title-font font-medium text-gray-500 tracking-wider">Flush All</h3>
+					<p class="text-gray-500">Clear all cache from DB</p>
+				</div>
+				<div>
+					<p class="text-red-500">{flushed ? flushed : ''}</p>
+				</div>
+				<button
+					class="flex text-white bg-red-500 border-0 py-2 px-8 focus:outline-none hover:bg-red-600 rounded text-lg"
+					on:click={async () => (flushed = await useApi('flushAll'))}>Flush All</button
+				>
+			</div>
 		</div>
 	</div>
 </section>
